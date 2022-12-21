36bdcba8474e69dc2c4add8e165e75d7b7c30d93
O48WRX <kardyy81@gmail.com>
2022-12-21 17:15:43 +0100

    Fixed Java version of EJBLOCAL, Created Servlet1

--- Workspace/EJB/ejbModule/com/ekke/controllers/ProcesszorEJB.java
+++ Workspace/EJB/ejbModule/com/ekke/controllers/ProcesszorEJB.java
@@ -1,7 +1,11 @@
 package com.ekke.controllers;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+
+import com.ekke.entities.Processzor;
 
 /**
  * Session Bean implementation class ProcesszorEJB
@@ -16,5 +20,11 @@ public class ProcesszorEJB implements ProcesszorEJBLocal {
     public ProcesszorEJB() {
         // TODO Auto-generated constructor stub
     }
+
+	@Override
+	public List<Processzor> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Workspace/EJBClient/ejbModule/com/ekke/controllers/ProcesszorEJBLocal.java
+++ Workspace/EJBClient/ejbModule/com/ekke/controllers/ProcesszorEJBLocal.java
@@ -1,8 +1,12 @@
 package com.ekke.controllers;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.ekke.entities.Processzor;
 
 @Local
 public interface ProcesszorEJBLocal {
-
+	public List<Processzor> getList();
 }
