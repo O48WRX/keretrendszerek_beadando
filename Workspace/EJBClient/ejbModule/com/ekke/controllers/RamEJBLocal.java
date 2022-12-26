c2854bcbb661b08791386e87367f53baa5e51d1b
O48WRX <kardyy81@gmail.com>
2022-12-26 14:25:01 +0100

    RamEJBLocal defined

--- Workspace/EJBClient/ejbModule/com/ekke/controllers/RamEJBLocal.java
+++ Workspace/EJBClient/ejbModule/com/ekke/controllers/RamEJBLocal.java
@@ -1,8 +1,15 @@
 package com.ekke.controllers;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.ekke.entities.Ram;
 
 @Local
 public interface RamEJBLocal {
-
+	public List<Ram> getList();
+	public void deleteRam(int id);
+	public void updateRam(int id, String nev);
+	public void createRam(String nev);
 }
