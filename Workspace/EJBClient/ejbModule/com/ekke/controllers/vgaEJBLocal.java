252ce389f5b8abfe1473cfd49bbfcd3364716375
O48WRX <kardyy81@gmail.com>
2022-12-26 14:32:30 +0100

    VgaEJBLocal defined

--- Workspace/EJBClient/ejbModule/com/ekke/controllers/vgaEJBLocal.java
+++ Workspace/EJBClient/ejbModule/com/ekke/controllers/vgaEJBLocal.java
@@ -1,8 +1,15 @@
 package com.ekke.controllers;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.ekke.entities.Vga;
 
 @Local
 public interface vgaEJBLocal {
-
+	public List<Vga> getList();
+	public void deleteVga(int id);
+	public void updateVga(int id, String nev);
+	public void createVga(String nev);
 }
