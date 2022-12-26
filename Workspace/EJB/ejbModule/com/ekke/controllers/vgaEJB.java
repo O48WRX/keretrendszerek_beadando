58af5c20573117d1ff27ac9c2fdf20c71026b503
O48WRX <kardyy81@gmail.com>
2022-12-26 14:20:41 +0100

    vgaEJB & Local created

---
+++ Workspace/EJB/ejbModule/com/ekke/controllers/vgaEJB.java
@@ -0,0 +1,20 @@
+package com.ekke.controllers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class vgaEJB
+ */
+@Stateless(mappedName = "vgaEJB")
+@LocalBean
+public class vgaEJB implements vgaEJBLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public vgaEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
