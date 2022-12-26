991a4d9d71c2eb47341197445bf25a920461907c
O48WRX <kardyy81@gmail.com>
2022-12-26 14:29:46 +0100

    RigEJBLocal defined

--- Workspace/EJBClient/ejbModule/com/ekke/controllers/RigEJBLocal.java
+++ Workspace/EJBClient/ejbModule/com/ekke/controllers/RigEJBLocal.java
@@ -1,8 +1,18 @@
 package com.ekke.controllers;
 
+import java.util.List;
+
 import javax.ejb.Local;
+
+import com.ekke.entities.Processzor;
+import com.ekke.entities.Ram;
+import com.ekke.entities.Rig;
+import com.ekke.entities.Vga;
 
 @Local
 public interface RigEJBLocal {
-
+	public List<Rig> getList();
+	public void deleteRig(int id);
+	public void updateRig(int id, Processzor processzor, Ram ram, Vga vga);
+	public void createRig(Processzor processzor, Ram ram, Vga vga);
 }
